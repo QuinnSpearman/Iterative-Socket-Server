a1c17782db5f13a4585a04a215647d83aa64edda
Quinn Spearman <n01390826@unf.edu>
2020-06-23 13:14:54 -0400

    Up to 8 client requests possible. Stops working after 8.

--- Iterative Socket Server/src/Client.java
+++ Iterative Socket Server/src/Client.java
@@ -1,9 +1,43 @@
 import java.io.*;
 import java.net.*;
-public class Client {
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Client implements Runnable{
+	
+	static Socket socket;
+	static PrintWriter pr;
+	static String query;
+	String name;
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int port;
+		int clientQuantity;
+		
+		ArrayList<Thread> clients = new ArrayList<Thread>();
+		
+		System.out.print("Enter a port: ");
+		port = in.nextInt();
+		in.nextLine();
+		
 		try {
-			Socket socket = new Socket("139.62.210.153", 2020);
+			socket = new Socket("139.62.210.153", port);
+			pr = new PrintWriter(socket.getOutputStream());	
+			
+			System.out.println("Available commands: Time ");
+			System.out.print("Enter a command: ");		
+			query = in.nextLine();
+			
+			System.out.print("Number of clients: ");
+			clientQuantity = in.nextInt();
+			
+			for(int i = 0; i < clientQuantity; i++) {
+				clients.add(new Thread(new Client("Client " + (i + 1))));
+			}
+			
+			for(int i = 0; i < clientQuantity; i++) {
+				clients.get(i).start();
+			}
+					    
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -11,7 +45,27 @@ public class Client {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
+	
+	public Client(String x) {
+		name = x;
+	}
+	
+	public void run() {
+		pr.println(query);
+	    pr.flush();	    
+	    
+		try {
+			InputStreamReader inStreamReader = new InputStreamReader(socket.getInputStream());
+			BufferedReader bf = new BufferedReader(inStreamReader);
+	    
+			String message = bf.readLine();
+			System.out.println(name + ": " + message);    
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 
 }
--- Iterative Socket Server/src/Server.java
+++ Iterative Socket Server/src/Server.java
@@ -1,29 +1,52 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 public class Server {
 
 	public static void main(String[] args) {
-			
-		try {
-			ServerSocket serverSocket = new ServerSocket(2020);
-			Socket socket = serverSocket.accept();
+		Scanner in = new Scanner(System.in);
+		int port;
+		System.out.print("Enter a port: ");
+		port = in.nextInt();
+		String query;
 
+		try {
+			ServerSocket serverSocket = new ServerSocket(port);
+			Socket socket = serverSocket.accept();
 			
+			System.out.println("Client Connected.");
+			
+			InputStreamReader inStreamReader = new InputStreamReader(socket.getInputStream());
+			BufferedReader bf = new BufferedReader(inStreamReader);
+			
+			while(true) {
+				query = bf.readLine();
+				if(query == null) {
+					break;
+				}
+				
+				PrintWriter pr = new PrintWriter(socket.getOutputStream());
+				
+				if(query.equalsIgnoreCase("Time")) {
+					pr.println(dateAndTime());
+				}
+				pr.flush();
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-			System.out.println("Client Connected.");
-		
-		
-		
-
 	}
-	private static void dateAndTime() {
+	
+	
+	private static String dateAndTime() {
 		LocalDateTime dateTime = LocalDateTime.now();
-		System.out.println(dateTime.format(DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss")));		
+		return dateTime.format(DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss"));
 	}
+	
+
 }
+
