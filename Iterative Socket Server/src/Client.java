4114f77afcbfc68f4d4bdbce949cf9dd5c933eb3
Quinn Spearman <n01390826@unf.edu>
2020-06-24 16:28:55 -0400

    Commented version of previous commit.

--- Iterative Socket Server/src/Client.java
+++ Iterative Socket Server/src/Client.java
@@ -3,69 +3,85 @@ import java.net.*;
 import java.util.*;
 public class Client implements Runnable{
 	
+	// Public variable declarations
 	static String query;
 	String name;
 	static int port;
 	//static ArrayList<String> runtimes = new ArrayList<String>();
 	
 	public static void main(String[] args) {
+		
+		
 		Scanner in = new Scanner(System.in);
 		
+		// Number of clients being used
 		int clientQuantity;
+		
+		//ArrayList which is used to store each thread
 		ArrayList<Thread> clients = new ArrayList<Thread>();
 		
+		// User enters a port number which is stored in port
 		System.out.print("Enter a port: ");
 		port = in.nextInt();
 		in.nextLine();
-		
-
-			
+	
+			// User enters which query they want to request
 			System.out.println("Available commands: Time ");
 			System.out.print("Enter a command: ");		
 			query = in.nextLine();
 			
+			// User enters the number of clients they want to spawn
 			System.out.print("Number of clients: ");
 			clientQuantity = in.nextInt();
 			
+			
+			// Creates a thread object and adds it to the clients array
+			// (one for each client)
 			for(int i = 0; i < clientQuantity; i++) {
 				clients.add(new Thread(new Client("Client " + (i + 1))));
 			}
 			
+			// Runs the run function once for each client
+			// Threads are run simultaneously 
 			for(int i = 0; i < clientQuantity; i++) {
 				clients.get(i).start();
-			}
-			
-			
-			
-			//Collections.sort(runtimes);
-			
-			/*for(int i = 0; i < clientQuantity; i++) {
-				System.out.println(runtimes.get(i));
-			}*/
+			}			
 											 		
 	}
 	
+	// Client object constructor
 	public Client(String x) {
 		name = x;
 	}
 	
+	// Run function which runs a separate thread for every client
 	public void run() {
 
-	    
 		try {
+			
+			// Starts a system timer
 			long start = System.nanoTime();
+			// Creates a socket object
 			Socket socket = new Socket("139.62.210.153", port);
+			// Creates a Print Writer object
 			PrintWriter pr = new PrintWriter(socket.getOutputStream());	
 			
+			// Sends the query to the server and flushes the stream
 			pr.println(query);
 		    pr.flush();
 			
+		    // Initializes the input stream reader object needed for the Buffered Reader object
 			InputStreamReader inStreamReader = new InputStreamReader(socket.getInputStream());
+			// Buffered Reader object used for reading messages from the server
 			BufferedReader bf = new BufferedReader(inStreamReader);
 	    
+			// Reads the message from the server
 			String message = bf.readLine();
+			// Stops the timer
 			long stop = System.nanoTime();
+			// Displays the message 
 			System.out.println(name + ": " + message);
+			// Calculates and displays the runtime in ms of each client query
 			System.out.println(name + ": " + ((stop - start) / 1000000) + "ms");
 			//runtimes.add(name + ": " + ((stop - start) / 1000000) + "ms");
 		} catch (Exception e) {
--- Iterative Socket Server/src/Server.java
+++ Iterative Socket Server/src/Server.java
@@ -8,37 +8,54 @@ public class Server {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int port;
+		String query;
+		
+		// Prompts user for a port number and stores input in port variable
 		System.out.print("Enter a port: ");
 		port = in.nextInt();
-		String query;
+		
 
 		try {
+			// Creates new Server Socket object and 
 			ServerSocket serverSocket = new ServerSocket(port);
 			System.out.println("Server is listening on port: " + port);
 			
-			
+			// While loop that assures that as long as the server is up, it is listening for client requests 
 			while(true) {				
+				
+				// Starts listening for requests from clients
 				Socket socket = serverSocket.accept();
 				
+				// Initializes the input stream reader object needed for the Buffered Reader object
 				InputStreamReader inStreamReader = new InputStreamReader(socket.getInputStream());
+				// Buffered Reader object used for reading input from the client
 				BufferedReader bf = new BufferedReader(inStreamReader);
+				// Print Writer object used for sending information to the client
 				PrintWriter pr = new PrintWriter(socket.getOutputStream());
 				
+				// Notifies the server administrator that a new client has been connected 
+				// (only prints on the server and mostly used for debugging purposes)
 				System.out.println("New client connected.");
 				
+				// Reads a query from the client
 				query = bf.readLine();
 		
+				// Checks which query was requested by the user
 				if(query.equalsIgnoreCase("Time")) {
+					// Sends the date and time back to the user
 					pr.println(dateAndTime());
 				}
+				
+				// Flushes the Print Reader stream
 				pr.flush();
 				
+				// Closes the socket and notifies the server administrator of the closure of the socket
 				socket.close();
 				System.out.println("Socket closed.");
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 
 	}
