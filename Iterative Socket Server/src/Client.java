c73cf233c9c9cbca3bb7daaed1cb66adaf51045e
Quinn Spearman <n01390826@unf.edu>
2020-06-26 16:52:59 -0400

    Fully commented. Complete except for user inputting IP address.

--- Iterative Socket Server/src/Client.java
+++ Iterative Socket Server/src/Client.java
@@ -1,12 +1,13 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.lang.*;
 
 public class Client implements Runnable {
 
-	// Public variable declarations
+	// Global variable declarations
 	static String query;
-	String name;
+	String clientName;
 	static int port;
 	static int clientQuantity;
 	static int totalRuntime = 0;
@@ -19,7 +20,7 @@ public class Client implements Runnable {
 		// ArrayList which is used to store each thread
 		ArrayList<Thread> clients = new ArrayList<Thread>();
 
-		// User enters a port number which is stored in port
+		// User enters a port number which is stored in port variable
 		System.out.print("Enter a port: ");
 		port = in.nextInt();
 		in.nextLine();
@@ -27,7 +28,7 @@ public class Client implements Runnable {
 		// User enters which query they want to request
 		System.out.println("Available commands: Date and Time | Uptime | Memory Use | Netstat | Current Users | Running Processes");
 		System.out.print("Enter a command: ");
-		query = in.nextLine();
+		query = in.nextLine();		
 
 		// User enters the number of clients they want to spawn
 		System.out.print("Number of clients: ");
@@ -35,40 +36,42 @@ public class Client implements Runnable {
 
 		// Creates a thread object and adds it to the clients array
 		// (one for each client)
-		for (int i = 0; i < clientQuantity; i++) {
-			clients.add(new Thread(new Client("Client " + (i + 1))));
+		for (int i = 1; i <= clientQuantity; i++) {
+			clients.add(new Thread(new Client("Client " + (i))));
 		}
 
 		// Runs the run function once for each client
-		// Threads are run simultaneously
+		// (Threads are run simultaneously)
 		for (int i = 0; i < clientQuantity; i++) {
 			clients.get(i).start();
 		}
-
 	}
 
 	// Client object constructor
-	public Client(String x) {
-		name = x;
+	public Client(String name) {
+		clientName = name;
 	}
 
-	// Run function which runs a separate thread for every client
+	// Run function which runs a separate thread for every client simultaneously
 	public void run() {
 		
-		long runtime = 0;
+		// Stores runtime of client query
+		long runtime;
+		
+		// Helps build the string of the message sent from the server
 		StringBuilder message = new StringBuilder();
 		
 		try {
-			// Starts a system timer
-			long start = System.nanoTime();
+			// Stores the current system time in nanoseconds 
+			long start = System.currentTimeMillis();
 			// Creates a socket object
 			Socket socket = new Socket("139.62.210.153", port);
 			// Creates a Print Writer object
-			PrintWriter pr = new PrintWriter(socket.getOutputStream());
+			PrintWriter querySender = new PrintWriter(socket.getOutputStream());
 
 			// Sends the query to the server and flushes the stream
-			pr.println(query);
-			pr.flush();
+			querySender.println(query);
+			querySender.flush();
 
 			// Initializes the input stream reader object needed for the Buffered Reader
 			// object
@@ -76,27 +79,33 @@ public class Client implements Runnable {
 			// Buffered Reader object used for reading messages from the server
 			BufferedReader bf = new BufferedReader(inStreamReader);
 
-			// Reads the message from the server
+			// String variable for reading each line from server message individually
 			String line;
+			
+			//Checks if the line being read from the server is not null, if so append the line to the message with a new line
 			while((line = bf.readLine()) != null) {
 				message.append(line + "\n");
 			}
+			
+			// closes the socket
+			socket.close();
 
-			// Stops the timer
-			long stop = System.nanoTime();
+			// Stores the system time in milliseconds
+			long stop = System.currentTimeMillis();
+			
 			// Displays the message
-			System.out.println(name + ": " + message);
+			System.out.println(clientName + ": " + message);
 
 			// Calculates the runtime of the query in milliseconds
-			runtime = ((stop - start) / 1000000);
+			runtime = stop - start;
 			
 			// Sums the total runtime of all clients
 			totalRuntime += runtime;
 			
 			// Adds the runtime to the runtimes array
-			runtimes.add(name + " runtime: " + runtime + "ms");
+			runtimes.add(clientName + " runtime: " + runtime + "ms");
 			
-			// If this is the final client request, get the runtime
+			// If this is the final client request, display all of the runtimes
 			if (clientQuantity <= runtimes.size()) {
 				displayRuntimes();
 			}
@@ -107,13 +116,17 @@ public class Client implements Runnable {
 		}
 	}
 	
-	// Displays the runtimes in the form of individual, total, and average
+	// Displays the individual, total, and average runtimes
 	private void displayRuntimes() {
+		
+		// Sorts all client query runtimes in order of client number
 		Collections.sort(runtimes);
 		
+		// displays all of the client runtimes
 		for (int i = 0; i < clientQuantity; i++) {					
 			System.out.println(runtimes.get(i));
 		}
+		// displays total and average runtimes of clients
 		System.out.println("Total runtime: " + totalRuntime + "ms");
 		System.out.println("Average runtime: " + totalRuntime / clientQuantity + "ms");
 	}
--- Iterative Socket Server/src/Server.java
+++ Iterative Socket Server/src/Server.java
@@ -1,136 +1,132 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
-import java.lang.Runtime;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.lang.*;
+import java.time.*;
+
 public class Server {
-	
+
 	public static void main(String[] args) {
-			
+
+		// Scanner object for taking user input of port number
 		Scanner in = new Scanner(System.in);
+
+		// Local variable declarations
 		int port;
 		String query;
-		
+		String message = "";
+
 		// Prompts user for a port number and stores input in port variable
 		System.out.print("Enter a port: ");
 		port = in.nextInt();
-		
 
 		try {
-			// Creates new Server Socket object and 
+
+			// Creates Server socket object attached to the requested port
 			ServerSocket serverSocket = new ServerSocket(port);
+			// Displays which port number the server is listening on
 			System.out.println("Server is listening on port: " + port);
-			
-			// While loop that assures that as long as the server is up, it is listening for client requests 
-			while(true) {				
-				
-				StringBuilder message = new StringBuilder();
-				
+
+			// While loop that assures that as long as the server is up, it is listening for
+			// client requests
+			while (true) {
+
 				// Starts listening for requests from clients
 				Socket socket = serverSocket.accept();
-				
-				// Initializes the input stream reader object needed for the Buffered Reader object
+
+				// Initializes the input stream reader object needed for the Buffered Reader
+				// object
 				InputStreamReader inStreamReader = new InputStreamReader(socket.getInputStream());
-				// Buffered Reader object used for reading input from the client
-				BufferedReader bf = new BufferedReader(inStreamReader);
+
+				// Buffered Reader object used for reading queries from the client
+				BufferedReader queryReader = new BufferedReader(inStreamReader);
+
 				// Print Writer object used for sending information to the client
 				PrintWriter pr = new PrintWriter(socket.getOutputStream());
-				
-				// Notifies the server administrator that a new client has been connected 
-				// (only prints on the server and mostly used for debugging purposes)
-				System.out.println("New client connected.");
-				
+
 				// Reads a query from the client
-				query = bf.readLine(); //check converting this straight to lowercase
-				
+				query = queryReader.readLine(); // check converting this straight to lowercase
+
 				query = query.toLowerCase();
-				
-				
-				switch(query) {
-					case "date and time":
-						query = "date";
-						break;
-					case "memory use":
-						query = "free -h";
-						message.append("\n");
-						break;
-					case "current users":
-						query = "w";
-						break;
-					case "running processes":
-						query = "ps -aux";
-						message.append("\n");
-					case "uptime":
-					case "netstat":
-						break;
+
+				// Displays that a query has been received and what the query is
+				System.out.println("Query received: " + query);
+
+				// Determines which command is being requested, gets the command output from the
+				// getCommand function and stores it in message
+				switch (query) {
+				case "date and time":
+					message = getCommand("date");
+					break;
+				case "memory use":
+					message = "\n" + getCommand("free -h");
+					break;
+				case "current users":
+					message = getCommand("w");
+					break;
+				case "running processes":
+					message = "\n" + getCommand("ps -aux");
+					break;
+				case "uptime":
+				case "netstat":
+					message = getCommand(query);
+					break;
+				default:
+					message = "Invalid query.";
+					break;
 				}
-					
-					//ProcessBuilder processBuilder = new ProcessBuilder();
-					
-					//processBuilder.command(query);
-					
-					Process process = Runtime.getRuntime().exec(query);
-					
-					BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));		
-					
-					String line;
-					while((line = reader.readLine()) != null) {
-						message.append(line + "\n");
-					}
-				
-				pr.println(message.toString());
-				
-				// Checks which query was requested by the user
-				/*if(query.equalsIgnoreCase("Time")) {
-					// Sends the date and time to the user
-					
-					processBuilder.command("date");
-					
-					Process process = processBuilder.start();
-					
-					BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-					
-					String line;
-					while((line = reader.readLine()) != null) {
-						message += line;
-					}
-					
-					pr.println(message);
-				}
-				else if(query.equalsIgnoreCase("Uptime")) {
-					// Sends the uptime to the user
-					//pr.println(uptime());
-				}
-				else if(query.equalsIgnoreCase("Memory Usage")) {
-					pr.println(memoryUsage());
-				}*/
-				
-				// Flushes the Print Reader stream
+
+				pr.println(message);
 				pr.flush();
-				
-				
-				// Closes the socket and notifies the server administrator of the closure of the socket
+
+				// Displays that the response has been transmitted and what that response is.
+				System.out.println("Response transmitted: " + query + "\n");
+
+				// Flushes the print writer stream
+
+				// Closes the socket
 				socket.close();
-				System.out.println("Socket closed.");
 			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			e.printStackTrace();
 		}
 
 	}
-	
-	// Returns a string with the current date and time on the server
-	private static String dateAndTime() {
-		LocalDateTime dateTime = LocalDateTime.now();
-		return dateTime.format(DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss"));
+
+	// Passes in the linux command as a string and outputs the linux command output
+	// as a stringF
+	private static String getCommand(String query) {
+
+		// String builder object declaration, will be used for creating message that
+		// will be sent back to user
+		StringBuilder message = new StringBuilder();
+
+		try {
+
+			// Process object which calls for Linux command and stores it
+			Process process = Runtime.getRuntime().exec(query);
+
+			// Buffered reader which reads the Linux command
+			BufferedReader commandOutputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+			// String which will be used to temporarily store each line of the linux command
+			// output
+			String line;
+
+			// While there is still a Linux command output line to be read, store it in line
+			// and append that line to the message variable followed by a new line
+			while ((line = commandOutputReader.readLine()) != null) {
+				message.append(line + "\n");
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return message.toString();
+
 	}
-	
-	// Returns a string with the elapsed time which the server has been running
-	
-	private static String memoryUsage() {
-		return "Memory usage " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024  + "Kb";
-	}
-	
 }
